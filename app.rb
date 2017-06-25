113dd377fc17369745fd3f6f635d18ba7c09810b
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-06-25 13:18:47 +0100

    basic game structure

--- app.rb
+++ app.rb
@@ -3,9 +3,26 @@ require_relative './lib/game'
 
 class AppleTree < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
-    @game = Game.new
     erb :index
+  end
+
+  post '/game/new' do
+    session[:game] = Game.new
+    redirect '/game'
+  end
+
+  get '/game' do
+    @game = session[:game]
+    erb :game
+  end
+
+  post '/guess' do
+    game = session[:game]
+    game.guess(params[:guess])
+    redirect '/game'
   end
 
   run! if app_file == $0
