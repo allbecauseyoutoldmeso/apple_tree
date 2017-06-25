d6a5d7d22db6bdd692463bef235bdfe05ad2bfbf
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-06-25 23:20:50 +0100

    remove redundant code from WordMaker

--- lib/word_maker.rb
+++ lib/word_maker.rb
@@ -1,11 +1,7 @@
 require 'net/http'
 
 class WordMaker
-
-  def uri(length)
-    URI('http://setgetgo.com/randomword/get.php?len=' + length.to_s)
-  end
-
+  
   def get_word(length)
     uri = URI('http://setgetgo.com/randomword/get.php?len=' + length.to_s)
     Net::HTTP.get(uri).downcase
