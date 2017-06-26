030460228a5f761253855f83c77293539b866106
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-06-26 12:06:33 +0100

    test and write get_definition method

--- lib/word_maker.rb
+++ lib/word_maker.rb
@@ -1,10 +1,22 @@
 require 'net/http'
+require 'json'
 
 class WordMaker
-  
+
   def get_word(length)
     uri = URI('http://setgetgo.com/randomword/get.php?len=' + length.to_s)
     Net::HTTP.get(uri).downcase
+  end
+
+  def get_definition(word)
+    uri = URI.parse('https://od-api.oxforddictionaries.com:443/api/v1/entries/en/' + word)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request['app_id'] = '1999668e'
+    request['app_key'] = '193c4f346db04dbe4f2bf28efd7ee8b2'
+    response = http.request(request)
+    JSON.parse(response.body)['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]
   end
 
 end
--- spec/word_maker_spec.rb
+++ spec/word_maker_spec.rb
@@ -4,12 +4,18 @@ describe WordMaker do
 
   subject(:word_maker) { described_class.new }
 
-  describe '#get word' do
+  describe '#get_word' do
     it 'returns a word' do
       expect(word_maker.get_word(6)).to be_a(String)
     end
     it 'returns a word of the specified length' do
       expect(word_maker.get_word(6).length).to eq 6
+    end
+  end
+
+  describe '#get_definition' do
+    it 'returns the definition of a word' do
+      expect(word_maker.get_definition('toaster')).to eq 'an electrical device for making toast.'
     end
   end
 
