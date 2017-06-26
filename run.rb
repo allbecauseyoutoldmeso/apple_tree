0624c1c4e7f77cf683923dc934df7843176f414c
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-06-26 11:53:42 +0100

    experimental extraction of definition from json object

--- run.rb
+++ run.rb
@@ -1,8 +1,8 @@
 require 'net/http'
-require 'uri'
 require 'openssl'
+require 'json'
 
-uri = URI.parse('https://od-api.oxforddictionaries.com:443/api/v1/entries/en/ace')
+uri = URI.parse('https://od-api.oxforddictionaries.com:443/api/v1/entries/en/kidney')
 
 http = Net::HTTP.new(uri.host, uri.port)
 http.use_ssl = true
@@ -12,10 +12,8 @@ request = Net::HTTP::Get.new(uri.request_uri)
 request['Accept'] = 'application/json'
 request['app_id'] = '1999668e'
 request['app_key'] = '193c4f346db04dbe4f2bf28efd7ee8b2'
-# request.basic_auth 'Accept', 'application/json'
-# request.basic_auth 'app_id', '1999668e'
-# request.basic_auth 'app_key', '193c4f346db04dbe4f2bf28efd7ee8b2'
 
 response = http.request(request)
 
-puts response.body
+all_data = JSON.parse(response.body)
+puts all_data['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]
